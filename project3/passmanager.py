a46436186ab42b88b9c05168d76fe5931eda6c4f
Precioux <uni.mahdipour@gmail.com>
2024-01-28 11:57:24 +0330

    project3 - part1 - add and show

--- project3/passmanager.py
+++ project3/passmanager.py
@@ -11,18 +11,21 @@ parser.add_argument("--newpass", help="Create a new password", nargs=4)
 parser.add_argument("--showpass", action="store_true", help="Show saved passwords")
 parser.add_argument("--sel", help="Select a password by name")
 parser.add_argument("--update", help="Update a password", nargs=2)
-parser.add_argument("--del", help="Delete a password by name")
+parser.add_argument("--delete", help="Delete a password by name")
 args = parser.parse_args()
+
 
 # Step 3: Define functions for encryption and decryption
 def derive_key(password, salt=b'some_salt', iterations=100000, key_length=32):
     key = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, iterations, key_length)
     return key
 
+
 def encrypt(text, key):
     cipher = AES.new(key, AES.MODE_EAX)
     ciphertext, tag = cipher.encrypt_and_digest(text.encode())
     return base64.b64encode(cipher.nonce + tag + ciphertext).decode()
+
 
 def decrypt(encrypted_text, key):
     data = base64.b64decode(encrypted_text.encode())
@@ -40,15 +43,15 @@ def save_password(name, password, comment, key):
     with open(passwords_file, "a") as file:
         entry = {"name": name, "password": encrypted_password, "comment": comment}
         file.write(json.dumps(entry) + "\n")
-    return  encrypted_password
+    return encrypted_password
 
 
-def show_passwords(key):
+def show_passwords():
     with open(passwords_file, "r") as file:
         for line in file:
             entry = json.loads(line)
             name = entry["name"]
-            decrypted_password = decrypt(entry["password"], key)
+            decrypted_password = entry["password"]
             comment = entry["comment"]
             print(f"Name: {name}, Password: {decrypted_password}, Comment: {comment}")
 
@@ -87,14 +90,12 @@ def delete_password(name):
 if args.newpass:
     name, comment, password, key = args.newpass
     new_key = derive_key(key)
-    # print(f'name: {name}\ncomment: {comment}\nkey: {key}\npassword: {password}\n')
     encrypted_password = save_password(name, password, comment, new_key)
     print('New Password added!')
     print(f'Encrypted Password: {encrypted_password}')
 elif args.showpass:
-    key = input("Enter your simple password: ")
-    key = derive_key(key)
-    show_passwords(key)
+    print('Showing Passwords..')
+    show_passwords()
 elif args.sel:
     name, key = args.sel, input("Enter your simple password: ")
     key = derive_key(key)
